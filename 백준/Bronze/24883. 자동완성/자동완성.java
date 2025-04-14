554ad7b8a89328e638d9ec82555b786633e819f4
kimhouyoung <kim744200816@gmail.com>
2025-04-15 08:45:33 +0900

    [Bronze V] Title: 자동완성, Time: 168 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		if(str.equals("N")) {
+			System.out.println("Naver D2");
+		}
+		else if(str.equals("n")) {
+			System.out.println("Naver D2");
+		}
+		else {
+			System.out.println("Naver Whale");
+		}
+		
+	}
+
+}
+    
